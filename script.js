1f87467f11eccb41ad89b865b02ee071311d6900
Frank Albenesius <frankalbenesius@gmail.com>
2017-02-22 19:22:44 -0600

    add neat css, links to timestamps

--- script.js
+++ script.js
@@ -21,13 +21,13 @@ function toMinutes(seconds) {
   return padLeft(minutes, '0', 2)+':'+padLeft(secondsLeft, '0', 2);
 }
 
-function createElement(element, className, content) {
-  // creates an element
-  content = content || ''; // default empty content
-  var element = document.createElement(element);
-  element.className = className;
-  element.innerHTML = content;
-  return element;
+function setCurrentTime(seconds) {
+  console.log('seconds', seconds);
+  var leadIn = Math.max(seconds - 2, 0); // start a bit before actual cue time
+  console.log('leadIn', leadIn);
+  player.setCurrentTime(leadIn).catch(function(error) {
+    console.warn('an error occurred setting the player time');
+  });
 }
 
 function removeCuePoint(id) {
@@ -49,37 +49,66 @@ function removeCuePoint(id) {
   });
 }
 
+function createElement(element, className, content) {
+  // creates an element
+  content = content || ''; // default empty content
+  var element = document.createElement(element);
+  element.className = className;
+  element.innerHTML = content;
+  return element;
+}
+
 function createCueListItem(id, seconds, text) {
   // creates a cue list item
   var cueListItem = createElement('div', 'cueListItem');
-  cueListItem.appendChild(createElement('div', 'cueListItemTime', toMinutes(seconds)));
-  cueListItem.appendChild(createElement('div', 'cueListItemText', text));
+
+  var timeButton = createElement('button', 'cueListItemTime', toMinutes(seconds));
+  timeButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    setCurrentTime(seconds);
+  });
+  cueListItem.appendChild(timeButton);
+
   var deleteButton = createElement('button', 'cueListItemDelete', 'Delete')
   deleteButton.addEventListener('click', function(event) {
     event.preventDefault();
     removeCuePoint(id);
   });
   cueListItem.appendChild(deleteButton);
+
+  var textWrapper = createElement('div', 'cueListItemTextWrapper');
+  textWrapper.appendChild(createElement('div', 'cueListItemText', text));
+  cueListItem.appendChild(textWrapper);
+
   return cueListItem;
 }
 
 function clearCueList() {
-  // clears cueListItems
+  // clears cueListItems && no cues message
   var currentItems = document.getElementsByClassName('cueListItem');
   while(currentItems[0]) { // remove current list items (items are removed in live list)
     currentItems[0].parentNode.removeChild(currentItems[0]);
   }
 }
 
+var noCues = document.getElementById('noCues');
 function refreshCueList() {
   // gets current video cues and displays them
   clearCueList();
   player.getCuePoints().then(function(cuePoints) {
-    for(var i=0; i < cuePoints.length; i++) {
-      var cuePoint = cuePoints[i];
-      document.getElementById('cueList').appendChild(
-        createCueListItem(cuePoint.id, cuePoint.time, cuePoint.data.text)
-      );
+    if (cuePoints.length < 1) {
+      noCues.style.display = 'block';
+    } else {
+      noCues.style.display = 'none';
+      cuePoints.sort(function(a, b) { //sort this just in case
+        return a.time - b.time;
+      });
+      for(var i=0; i < cuePoints.length; i++) {
+        var cuePoint = cuePoints[i];
+        document.getElementById('cueList').appendChild(
+          createCueListItem(cuePoint.id, cuePoint.time, cuePoint.data.text)
+        );
+      }
     }
   }).catch(function(error) {
     console.warn('getCuePoints()', error);
@@ -95,25 +124,27 @@ document.getElementById('cueForm').addEventListener('submit', function(e) {
   player.getCurrentTime().then(function(seconds) {
     var time = seconds;
     var text = document.getElementById('cueInput').value;
-    document.getElementById('cueInput').value = ''; // clear input value
+    if (text) { // do nothing if there is not text for the cue
+      document.getElementById('cueInput').value = ''; // clear input value
 
-    player.addCuePoint(time, {
-      text: text }
-    ).then(function(id) {
-      refreshCueList()
-    }).catch(function(error) {
-      switch (error.name) {
-        case 'UnsupportedError':
-          console.warn('cue points are not supported with the current player or browser', error);
-          break;
-        case 'RangeError':
-          console.warn('the time was less than 0 or greater than the video’s duration', error);
-          break;
-        default:
-          console.warn('some other error occurred', error);
-          break;
-      }
+      player.addCuePoint(time, {
+        text: text }
+      ).then(function(id) {
+        refreshCueList()
+      }).catch(function(error) {
+        switch (error.name) {
+          case 'UnsupportedError':
+            console.warn('cue points are not supported with the current player or browser', error);
+            break;
+          case 'RangeError':
+            console.warn('the time was less than 0 or greater than the video’s duration', error);
+            break;
+          default:
+            console.warn('some other error occurred', error);
+            break;
+        }
     });
+    }
   }).catch(function(error) {
     console.warn('getCurrentTime()', error);
   });
